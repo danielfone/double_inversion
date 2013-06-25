61baf9e2e1c235791466f6a31ce33e8eed2bdb53
Daniel Fone <daniel@fone.net.nz>
2013-06-25 21:22:29 +1200

    assert with raise

--- inversion.rb
+++ inversion.rb
@@ -1,9 +1,15 @@
 #!/usr/bin/env ruby
 
+def assert_returns(input, actual, expected)
+  raise "Expected #{input} to produce #{expected}: got #{actual}" if actual != expected
+end
+
+#---
+
 def f(n)
   
 end
 
 (-10..10).each do |n|
-  assert_returns :f, n, (-1 * n)
+  assert_returns n, f(n), (-1 * n)
 end
