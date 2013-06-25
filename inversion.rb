3f57beeaef61625e2a354ed26a6bbcd06671036a
Daniel Fone <daniel@fone.net.nz>
2013-06-25 21:31:14 +1200

    Refactor into class

--- inversion.rb
+++ inversion.rb
@@ -1,25 +1,47 @@
 #!/usr/bin/env ruby
 
-def assert_returns(input, actual, expected)
-  if actual == expected
-    @pass_count += 1
-    puts_pass "#{input.inspect} => #{actual.inspect}"
-  else
-    @fail_count += 1
-    puts_fail "Expected #{input.inspect} => #{expected.inspect}: got #{actual.inspect}"
+class MicroTest
+
+  def self.test
+    yield test = new
+    test.puts_totals
   end
-end
 
-def puts_pass(message)
-  puts_color message, '32'
-end
+  def initialize
+    @pass_count = 0
+    @fail_count = 0
+  end
 
-def puts_fail(message)
-  puts_color message, '31'
-end
+  def puts_totals
+    puts "---"
+    puts_pass "Passed: #{@pass_count}" if @pass_count > 0
+    puts_fail "Failed: #{@fail_count}" if @fail_count > 0
+  end
 
-def puts_color(message, color_code)
-  puts "\e[0;#{color_code}m" << message << "\e[0m"
+  def assert_returns(input, actual, expected)
+    if actual == expected
+      @pass_count += 1
+      puts_pass "#{input.inspect} => #{actual.inspect}"
+    else
+      @fail_count += 1
+      puts_fail "Expected #{input.inspect} => #{expected.inspect}: got #{actual.inspect}"
+    end
+  end
+
+private
+
+  def puts_pass(message)
+    puts_color message, '32'
+  end
+
+  def puts_fail(message)
+    puts_color message, '31'
+  end
+
+  def puts_color(message, color_code)
+    puts "\e[0;#{color_code}m" << message << "\e[0m"
+  end
+
 end
 
 #---
@@ -28,13 +50,10 @@ def f(n)
   -10
 end
 
-@pass_count = 0
-@fail_count = 0
-
-(-10..10).each do |n|
-  assert_returns n, f(n), (-1 * n)
+MicroTest.test do |test|
+  (-10..10).each do |n|
+    test.assert_returns n, f(n), (-1 * n)
+  end
 end
 
-puts "---"
-puts_pass "Passed: #{@pass_count}" if @pass_count > 0
-puts_fail "Failed: #{@fail_count}" if @fail_count > 0
+
