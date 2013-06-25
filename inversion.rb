28f21d849b4aead626b97ce05df1b2dd6cdfbb10
Daniel Fone <daniel@fone.net.nz>
2013-06-25 21:33:44 +1200

    Passing tests with lambda

--- inversion.rb
+++ inversion.rb
@@ -3,11 +3,16 @@
 require './micro_test'
 
 def f(n)
-  -10
+  case n
+  when Integer
+    lambda { n * -1 }
+  else
+    n.call
+  end
 end
 
 MicroTest.test do |test|
   (-10..10).each do |n|
-    test.assert_returns n, f(n), (-1 * n)
+    test.assert_returns n, f(f(n)), (-1 * n)
   end
 end
